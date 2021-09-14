c0ef1b4c5e335029b9a0f5e0c2ffa9d5076433f9
Jon Bell <j.to.the.bell@gmail.com>
2021-09-14 08:47:28 -0600

    Correct default step value to be fix16_one

--- lparser.c
+++ lparser.c
@@ -1381,7 +1381,7 @@ static void fornum (LexState *ls, TString *varname, int line) {
   if (testnext(ls, ','))
     exp1(ls);  /* optional step */
   else {  /* default step = 1 */
-    luaK_codek(fs, fs->freereg, luaK_numberK(fs, 1));
+    luaK_codek(fs, fs->freereg, luaK_numberK(fs, fix16_one));
     luaK_reserveregs(fs, 1);
   }
   forbody(ls, base, line, 1, 1);
